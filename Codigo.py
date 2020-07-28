b380853bece1c18e6bbbee55a9a7322c189ad9bc
unknown <axl@example.com>
2020-07-28 19:07:17 -0300

    se modifico Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,3 +1,11 @@
 class Producto:
 	def __init(self,nombre,precio):
+		self._nombre=nombre
+		self._precio=precio
+	def get_nombre(self):
+		return self._nombre
+	def precio(self):
+		if primera_necesidad():
+		
+			return 
 		pass
